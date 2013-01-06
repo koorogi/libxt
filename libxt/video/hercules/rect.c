4fda26a3968ad5ac09a1b342071e772ff99f513f
Bobby Bingham <uhmmmm@gmail.com>
2013-01-06 02:05:58 -0600

    hercules: faster screen clearing code
    OpenWatcom produces good assembly for _fmemset, but not for the
    equivalent loop.

--- libxt/video/hercules/rect.c
+++ libxt/video/hercules/rect.c
@@ -4,15 +4,9 @@
 #include <video/hercules.h>
 
 void xt_hercules_fill(XtHerculesFb buf, uint8_t fill) {
-    XtHerculesFb16 buf16 = (XtHerculesFb16) buf;
-    uint16_t      fill16 = fill | (fill << 8);
-
     for (int linemod4 = 0; linemod4 < 4; linemod4++) {
-        XtHerculesFb16 line = buf16;
-        for (int i = 0; i < (XT_HERCULES_GRAPHICS_ROWS >> 2) * (XT_HERCULES_GRAPHICS_COLS >> 4); i++) {
-            *line++ = fill16;
-        }
-        buf16 += 0x1000;
+        _fmemset(buf, fill, (XT_HERCULES_GRAPHICS_ROWS >> 2) * (XT_HERCULES_GRAPHICS_COLS >> 3));
+        buf += 0x2000;
     }
 }
 
