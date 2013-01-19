4e6726ee40a555eb70dcde2a4eeca5f4bfe6a335
Bobby Bingham <uhmmmm@gmail.com>
2013-01-19 13:12:41 -0600

    unittest: simplify bitshift test code
    Use macros to reduce massive code duplication.  We also get better
    debugging information for failed tests this way, including the input,
    output, and expected output.

--- unittest/bitshift.c
+++ unittest/bitshift.c
@@ -29,26 +29,34 @@ DEFINE_FUNC(asm_shr);
 DEFINE_FUNC(asm_sar_ax);
 DEFINE_FUNC(asm_sar_bx);
 
+#define SHIFTTEST1(fn,i,o)                              \
+    UNITTEST2(fn(i) == ((o) & 0xffff),                  \
+        "in = 0x%04" PRIx16 ", out = 0x%04" PRIx16 ", expected = 0x%04" PRIx16, \
+        (uint16_t) (i), (uint16_t) fn(i), (uint16_t) (o))
+
+#define SHIFTTEST(fn,i,op)              \
+    SHIFTTEST1(fn ##  1, i, i op  1);   \
+    SHIFTTEST1(fn ##  2, i, i op  2);   \
+    SHIFTTEST1(fn ##  3, i, i op  3);   \
+    SHIFTTEST1(fn ##  4, i, i op  4);   \
+    SHIFTTEST1(fn ##  5, i, i op  5);   \
+    SHIFTTEST1(fn ##  6, i, i op  6);   \
+    SHIFTTEST1(fn ##  7, i, i op  7);   \
+    SHIFTTEST1(fn ##  8, i, i op  8);   \
+    SHIFTTEST1(fn ##  9, i, i op  9);   \
+    SHIFTTEST1(fn ## 10, i, i op 10);   \
+    SHIFTTEST1(fn ## 11, i, i op 11);   \
+    SHIFTTEST1(fn ## 12, i, i op 12);   \
+    SHIFTTEST1(fn ## 13, i, i op 13);   \
+    SHIFTTEST1(fn ## 14, i, i op 14);   \
+    SHIFTTEST1(fn ## 15, i, i op 15);   \
+    SHIFTTEST1(fn ## 16, i, i op 16)
+
 static void shl(void) {
     UNITTEST_SECTION("Shift Left");
 
     for (uint32_t i = 0; i < 0x10000; i++) {
-        UNITTEST2(asm_shl1(i)  == ((i <<   1) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shl2(i)  == ((i <<   2) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shl3(i)  == ((i <<   3) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shl4(i)  == ((i <<   4) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shl5(i)  == ((i <<   5) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shl6(i)  == ((i <<   6) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shl7(i)  == ((i <<   7) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shl8(i)  == ((i <<   8) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shl9(i)  == ((i <<   9) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shl10(i) == ((i <<  10) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shl11(i) == ((i <<  11) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shl12(i) == ((i <<  12) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shl13(i) == ((i <<  13) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shl14(i) == ((i <<  14) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shl15(i) == ((i <<  15) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shl16(i) == ((i <<  16) & 0xffff), "i = 0x%04x", (uint16_t) i);
+        SHIFTTEST(asm_shl, i, <<);
     }
 
     UNITTEST_SECTION_END();
@@ -58,22 +66,7 @@ static void shr(void) {
     UNITTEST_SECTION("Shift Right");
 
     for (uint32_t i = 0; i < 0x10000; i++) {
-        UNITTEST2(asm_shr1(i)  == i >>   1, "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shr2(i)  == i >>   2, "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shr3(i)  == i >>   3, "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shr4(i)  == i >>   4, "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shr5(i)  == i >>   5, "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shr6(i)  == i >>   6, "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shr7(i)  == i >>   7, "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shr8(i)  == i >>   8, "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shr9(i)  == i >>   9, "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shr10(i) == i >>  10, "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shr11(i) == i >>  11, "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shr12(i) == i >>  12, "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shr13(i) == i >>  13, "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shr14(i) == i >>  14, "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shr15(i) == i >>  15, "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_shr16(i) == i >>  16, "i = 0x%04x", (uint16_t) i);
+        SHIFTTEST(asm_shr, i, >>);
     }
 
     UNITTEST_SECTION_END();
@@ -83,41 +76,11 @@ static void sar_ax(void) {
     UNITTEST_SECTION("Arithmetic Shift Right (in AX)");
 
     for (uint32_t i = 0; i < 0x8000; i++) {
-        UNITTEST2(asm_sar_ax1(i)  == ((i >>   1) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax2(i)  == ((i >>   2) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax3(i)  == ((i >>   3) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax4(i)  == ((i >>   4) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax5(i)  == ((i >>   5) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax6(i)  == ((i >>   6) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax7(i)  == ((i >>   7) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax8(i)  == ((i >>   8) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax9(i)  == ((i >>   9) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax10(i) == ((i >>  10) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax11(i) == ((i >>  11) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax12(i) == ((i >>  12) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax13(i) == ((i >>  13) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax14(i) == ((i >>  14) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax15(i) == ((i >>  15) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax16(i) == ((i >>  16) & 0xffff), "i = 0x%04x", (uint16_t) i);
+        SHIFTTEST(asm_sar_ax, i, >>);
     }
 
     for (uint32_t i = 0xffffffff; i >= 0xffff8000; i++) {
-        UNITTEST2(asm_sar_ax1(i)  == ((i >>   1) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax2(i)  == ((i >>   2) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax3(i)  == ((i >>   3) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax4(i)  == ((i >>   4) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax5(i)  == ((i >>   5) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax6(i)  == ((i >>   6) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax7(i)  == ((i >>   7) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax8(i)  == ((i >>   8) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax9(i)  == ((i >>   9) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax10(i) == ((i >>  10) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax11(i) == ((i >>  11) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax12(i) == ((i >>  12) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax13(i) == ((i >>  13) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax14(i) == ((i >>  14) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax15(i) == ((i >>  15) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_ax16(i) == ((i >>  16) & 0xffff), "i = 0x%04x", (uint16_t) i);
+        SHIFTTEST(asm_sar_ax, i, >>);
     }
 
     UNITTEST_SECTION_END();
@@ -127,41 +90,11 @@ static void sar_bx(void) {
     UNITTEST_SECTION("Arithmetic Shift Right (in BX)");
 
     for (uint32_t i = 0; i < 0x8000; i++) {
-        UNITTEST2(asm_sar_bx1(i)  == ((i >>   1) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx2(i)  == ((i >>   2) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx3(i)  == ((i >>   3) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx4(i)  == ((i >>   4) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx5(i)  == ((i >>   5) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx6(i)  == ((i >>   6) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx7(i)  == ((i >>   7) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx8(i)  == ((i >>   8) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx9(i)  == ((i >>   9) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx10(i) == ((i >>  10) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx11(i) == ((i >>  11) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx12(i) == ((i >>  12) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx13(i) == ((i >>  13) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx14(i) == ((i >>  14) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx15(i) == ((i >>  15) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx16(i) == ((i >>  16) & 0xffff), "i = 0x%04x", (uint16_t) i);
+        SHIFTTEST(asm_sar_bx, i, >>);
     }
 
     for (uint32_t i = 0xffffffff; i >= 0xffff8000; i++) {
-        UNITTEST2(asm_sar_bx1(i)  == ((i >>   1) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx2(i)  == ((i >>   2) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx3(i)  == ((i >>   3) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx4(i)  == ((i >>   4) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx5(i)  == ((i >>   5) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx6(i)  == ((i >>   6) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx7(i)  == ((i >>   7) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx8(i)  == ((i >>   8) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx9(i)  == ((i >>   9) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx10(i) == ((i >>  10) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx11(i) == ((i >>  11) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx12(i) == ((i >>  12) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx13(i) == ((i >>  13) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx14(i) == ((i >>  14) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx15(i) == ((i >>  15) & 0xffff), "i = 0x%04x", (uint16_t) i);
-        UNITTEST2(asm_sar_bx16(i) == ((i >>  16) & 0xffff), "i = 0x%04x", (uint16_t) i);
+        SHIFTTEST(asm_sar_bx, i, >>);
     }
 
     UNITTEST_SECTION_END();
