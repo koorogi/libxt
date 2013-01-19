ee7c451b45d38c924bb8c76f5870a6e6f84a00df
Bobby Bingham <uhmmmm@gmail.com>
2013-01-18 21:11:55 -0600

    cosmetic: horizontal alignment

--- unittest/unittest.h
+++ unittest/unittest.h
@@ -6,22 +6,22 @@
 
 typedef void (*UnitTest)(void);
 
-#define UNITTEST_SECTION(name)  {           \
-    uint32_t total = 0, passed = 0;         \
+#define UNITTEST_SECTION(name)  {                       \
+    uint32_t total = 0, passed = 0;                     \
     printf("%s: %s\n", __FILE__, name);
 
-#define UNITTEST_SECTION_END()              \
+#define UNITTEST_SECTION_END()                          \
     printf("    %" PRIu32 " passed + %" PRIu32 " failed = %" PRIu32 " total\n\n", passed, total - passed, total); \
     }
 
-#define UNITTEST(expr)                      \
-    do {                                    \
-        _Bool pass = !!(expr);              \
-        passed    += pass;                  \
-        total++;                            \
-        if (!pass || verbose) {             \
+#define UNITTEST(expr)                                  \
+    do {                                                \
+        _Bool pass = !!(expr);                          \
+        passed    += pass;                              \
+        total++;                                        \
+        if (!pass || verbose) {                         \
             printf("    %s: %s\n", pass ? "PASS" : "FAIL", #expr ); \
-        }                                   \
+        }                                               \
     } while(0)
 
 #define UNITTEST2(expr,fmt,...)                         \
