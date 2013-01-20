33aa0e5a8ac48bc506061425758ccf5ce6c01510
Bobby Bingham <uhmmmm@gmail.com>
2013-01-20 15:11:28 -0600

    quaternion: clean up with better macros
    Improve the macros for accessing the input quaternion parameters, and
    create a similar macro for accessing the output quaternion.

--- libxt/math/quaternion2.asm
+++ libxt/math/quaternion2.asm
@@ -31,8 +31,9 @@ xt_quat_conj_:
     xpop    dx, di, bp
     ret     8
 
-%define quat1   bp+14
-%define quat2   bp+22
+%define QUAT1(offset)   [bp + offset + 14]
+%define QUAT2(offset)   [bp + offset + 22]
+%define RET(offset)     [si + offset]
 
 %define W       0
 %define X       2
@@ -48,23 +49,23 @@ xt_quat_conj_:
 ; clobbers ax, bx, cx, dx, di
 %macro qmult_stage  6
     ; cx = q1.SRC1a * q2.SRC2a
-    mov     ax, [quat1 + %1]    ; ax = di = q1.SRC1a
+    mov     ax, QUAT1(%1)   ; ax = di = q1.SRC1a
     mov     di, ax
-    mov     bx, [quat2 + %3]    ; bx = q2.SRC2a
+    mov     bx, QUAT2(%3)   ; bx = q2.SRC2a
     mult8x8 cx, bx
 
     ; ax = q1.SRC1a * q2.SRC2b
     ; cx = q1.SRC1a * q2.SRC2a
-    mov     ax, di              ; ax = q1.SRC1a
-    mov     dx, [quat2 + %4]    ; dx = di = q2.SRC2b
+    mov     ax, di          ; ax = q1.SRC1a
+    mov     dx, QUAT2(%4)   ; dx = di = q2.SRC2b
     mov     di, dx
     mult8x8 ax, dx
 
     ; ax = q1.SRC1b * q2.SRC2a
     ; bx = q1.SRC1a * q2.SRC2b
     ; cx = q1.SRC1a * q2.SRC2a
-    xchg    ax, bx              ; ax = q2.SRC2a; bx = q1.SRC1a * q2.SRC2b
-    mov     dx, es              ; dx = es = q1.SRC1b
+    xchg    ax, bx          ; ax = q2.SRC2a; bx = q1.SRC1a * q2.SRC2b
+    mov     dx, es          ; dx = es = q1.SRC1b
     mult8x8 ax, dx
 
     ; bx = q1.SRC1a * q2.SRC2b OP2 q1.SRC1b * q2.SRC2a
@@ -74,8 +75,8 @@ xt_quat_conj_:
     ; ax = q1.SRC1b * q2.SRC2b
     ; bx = q1.SRC1a * q2.SRC2b OP1 q1.SRC1b * q2.SRC2a
     ; cx = q1.SRC1a * q2.SRC2a
-    mov     ax, di              ; ax = q2.SRC2b
-    mov     dx, es              ; dx = q1.SRC1b
+    mov     ax, di          ; ax = q2.SRC2b
+    mov     dx, es          ; dx = q1.SRC1b
     mult8x8 ax, dx
 
     ; bx = q1.SRC1a * q2.SRC2b OP1 q1.SRC1b * q2.SRC2a
@@ -94,27 +95,27 @@ xt_quat_mult_:
     xpush       ax, bx, cx, dx, di, bp
     mov         bp, sp
 
-    mov         ax, [quat1 + X]
+    mov         ax, QUAT1(X)
     mov         es, ax
 
     qmult_stage W, X, W, X, sub, add
-    mov         [si + W], cx
-    mov         [si + X], bx
+    mov         RET(W), cx
+    mov         RET(X), bx
 
     qmult_stage W, X, Y, Z, sub, add
-    mov         [si + Y], cx
-    mov         [si + Z], bx
+    mov         RET(Y), cx
+    mov         RET(Z), bx
 
-    mov         ax, [quat1 + Z]
+    mov         ax, QUAT1(Z)
     mov         es, ax
 
     qmult_stage Y, Z, Y, Z, add, sub
-    sub         [si + W], cx
-    add         [si + X], bx
+    sub         RET(W), cx
+    add         RET(X), bx
 
     qmult_stage Y, Z, W, X, add, sub
-    add         [si + Y], cx
-    sub         [si + Z], bx
+    add         RET(Y), cx
+    sub         RET(Z), bx
 
     xpop        ax, bx, cx, dx, di, bp
     ret         16
